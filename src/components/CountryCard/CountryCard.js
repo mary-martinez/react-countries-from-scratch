40c5a208d3c837e9c952030717ec51080e083dfb
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-15 16:19:09 -0700

    Single static CountryCard is rendering

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function CountryCard() {
+export default function CountryCard({ name, iso2, continent }) {
+  const iso2Label = iso2.toLowerCase();
   return (
-    <div>
-
+    <div className="country-card">
+      <h3 className='country-name'>{name}</h3>
+      <img className='country-flag' src={`https://flagcdn.com/32x24/${iso2Label}.png`} />
+      <p className='continent'>{`Continent: ${continent}`}</p>
     </div>
   );
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchCountries } from '../services/countries';
+import CountryCard from '../components/CountryCard/CountryCard';
 
 export default function Main() {
   const [countries, setCountries] = useState([]);
@@ -11,12 +12,13 @@ export default function Main() {
     };
     fetchData();
   }, []);
-  console.log('countries', countries);
+  // console.log('countries', countries);
   return (
     <div>
-      {/* {countries.map((country) => ( */}
-      {/* // <CountryCard key={country.id} {...country} /> */}
-      {/* // )) */}
+      {/* {countries.map((country) => (
+        <CountryCard key={country.id} {...country} />
+      ))} */}
+      <CountryCard name='Andorra' iso2='AD' continent='Europe' />
     </div>
   );
 }
